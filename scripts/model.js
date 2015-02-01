21d2074c52ae25405241e195a5aa552a85f98cbd
Randal Parsons <randal_parsons@yahoo.com>
2015-02-01 20:14:23 +1100

    Initial use of Knockout for locations

--- scripts/model.js
+++ scripts/model.js
@@ -1,32 +1,3 @@
-/*
-function point(name, lat, long) {
-    this.name = name;
-    this.lat = ko.observable(lat);
-    this.long = ko.observable(long);
-
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(lat, long),
-        title: name,
-        map: map,
-        draggable: true
-    });
-
-    //if you need the poition while dragging
-    google.maps.event.addListener(marker, 'drag', function() {
-        var pos = marker.getPosition();
-        this.lat(pos.lat());
-        this.long(pos.lng());
-    }.bind(this));
-
-    //if you just need to update it when the user is done dragging
-    google.maps.event.addListener(marker, 'dragend', function() {
-        var pos = marker.getPosition();
-        this.lat(pos.lat());
-        this.long(pos.lng());
-    }.bind(this));
-}
-*/
-
 var locations = [
   { name: "North Bondi Ocean Pool", lat: -33.891492, lng: 151.282316 },
   { name: "Bondi Icebergs Pool", lat: -33.895028, lng: 151.274570 },
@@ -40,72 +11,57 @@ var locations = [
   { name: "Malabar Ocean Pool", lat: -33.968516, lng: 151.254552 }
 ];
 
-function ViewModel() {
-    var self = this;
+var viewModel = {
+  self: this,
 
-    self.initializeMap = function() {
+  initializeMap: function() {
 
-      var mapOptions = {
-        zoom: 12,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-   //       disableDefaultUI: true
-      };
+    var mapOptions = {
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.HYBRID
+ //       disableDefaultUI: true
+    };
 
-      self.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    self.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-      function pinLocation(id, location) {
+    function pinLocation(id, location) {
 
-        var bounds = window.mapBounds;
+      var marker = new google.maps.Marker({
+        map: self.map,
+        position: new google.maps.LatLng(location.lat, location.lng),
+        icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + id + '|00FF00|000000',
+        title: location.name
+      });
 
-        var marker = new google.maps.Marker({
-          map: self.map,
-          position: new google.maps.LatLng(location.lat, location.lng),
-          icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + id + '|00FF00|000000',
-          title: location.name
-        });
+      // TODO: Where to put more info???
+      var infoWindow = new google.maps.InfoWindow({
+        content: location.name
+      });
 
-        // infoWindows are the little helper windows that open when you click
-        // or hover over a pin on a map. They usually contain more information
-        // about a location.
-        var infoWindow = new google.maps.InfoWindow({
-          labelContent: id,
-          content: location.name
-        });
+      google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.open(self.map, marker);
+      });
 
-        google.maps.event.addListener(marker, 'click', function() {
-          infoWindow.open(self.map, marker);
-        });
-
-        bounds.extend(marker.position);
-
-        // fit the map to the new marker
-        //self.map.fitBounds(bounds);
-        // center the map
-        //self.map.setCenter(bounds.getCenter());
-      }
-
-      // Sets the boundaries of the map based on pin locations
-      window.mapBounds = new google.maps.LatLngBounds();
-      for (var i in locations) {
-        pinLocation(Number(i) + 1, locations[i]);
-      }
-      // fit the map to the new marker
-      self.map.fitBounds(window.mapBounds);
-      // center the map
-      self.map.setCenter(window.mapBounds.getCenter());
-
-      //pinPoster(locations);
+      window.mapBounds.extend(marker.position);
     }
-/*
-    points: ko.observableArray([
-        new point('Test1', 55, 11),
-        new point('Test2', 56, 12),
-        new point('Test3', 57, 13)]); */
+
+    // Sets the boundaries of the map based on pin locations
+    var bounds = window.mapBounds = new google.maps.LatLngBounds();
+
+    self.observableArray = ko.observableArray();
+    for (var i in locations) {
+      var location = locations[i];
+      self.observableArray.push(location);
+      pinLocation(Number(i) + 1, location);
+    }
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  }
 };
 
-var viewModel = new ViewModel();
-//ko.applyBindings(viewModel);
-window.addEventListener('load', viewModel.initializeMap);
-window.addEventListener('resize', function(e) {
+google.maps.event.addDomListener(window, 'load', viewModel.initializeMap);
+google.maps.event.addDomListener(window, 'resize', function(e) {
   viewModel.map.fitBounds(mapBounds);
 });
