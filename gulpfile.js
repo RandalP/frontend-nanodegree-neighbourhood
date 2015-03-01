c8b9c80ac70d85910c77d4073d91f82149e06378
Randal Parsons <randal_parsons@yahoo.com>
2015-03-01 22:50:25 +1100

    Implement gulp build. Add weather dialog.

---
+++ gulpfile.js
@@ -0,0 +1,150 @@
+'use strict';
+
+var gulp = require('gulp');
+var sequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var smoosher = require('gulp-smoosher');
+var print = require('gulp-print');
+var psi = require('psi');
+var site = '';
+var siteDir = './site';
+var portNum = 8080;
+
+gulp.task('css', function() {
+	var minifyCss = require('gulp-minify-css');
+
+	return gulp.src(['css/*'], { base: './' })
+    .pipe(minifyCss())
+    .pipe(gulp.dest(siteDir));
+});
+
+gulp.task('move', function() {
+  return gulp.src(['*.html'], { base: './' })
+    .pipe(gulp.dest(siteDir));
+});
+
+gulp.task('html', ['move'], function() {
+  var minifyHtml = require('gulp-minify-html');
+  var opts = { }; // comments: true, spare:true
+
+  return gulp.src(siteDir + '/*.html')
+    .pipe(smoosher({base : siteDir}))
+    .pipe(minifyHtml(opts))
+    .pipe(gulp.dest(siteDir));
+});
+
+gulp.task('img', function() {
+	var imagemin = require('gulp-imagemin');
+	var pngquant = require('imagemin-pngquant');
+  var mozjpeg = require('imagemin-mozjpeg');
+
+  return gulp.src(['img/*'], { base: './' })
+    .pipe(imagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant(), mozjpeg()]
+    }))
+    .pipe(gulp.dest(siteDir));
+});
+
+gulp.task('jshint', function() {
+    var stylish = require('jshint-stylish');
+    var jshint = require("gulp-jshint");
+
+  return gulp.src(['js/*'])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('js', function() {
+  var uglify = require('gulp-uglify');
+
+  return gulp.src(['js/*'], { base: './' })
+    .pipe(uglify())
+    .pipe(gulp.dest(siteDir));
+});
+
+gulp.task('browser-sync-psi', function() {
+	browserSync({
+    	port: portNum,
+    	open: false,
+        server: {
+        	baseDir: siteDir
+        }
+    });
+});
+
+gulp.task('ngrok-url', function(cb) {
+  var ngrok = require('ngrok');
+
+  return ngrok.connect(portNum, function (err, url) {
+      site = url;
+      console.log('serving your tunnel from: ' + site);
+      cb();
+  });
+});
+
+gulp.task('ngrok', function() {
+  return sequence(
+    'browser-sync-psi',
+    'ngrok-url');
+});
+
+gulp.task('psi-desktop', function () {
+  console.log("desktop: " + site);
+  return psi(site, {
+      nokey: 'true',
+      threshold: '90',
+      strategy: 'desktop'
+  }, function(err, data) {
+    console.log("desktop: " + data.score);
+    console.log(data.pageStats);
+  });
+ });
+
+gulp.task('psi-mobile', function () {
+  console.log("mobile: " + site);
+  return psi(site, {
+      nokey: 'true',
+      threshold: '90',
+      strategy: 'mobile'
+  }, function(err, data) {
+    console.log("mobile: " + data.score);
+    console.log(data.pageStats);
+  });
+});
+
+gulp.task('psi-seq', function (cb) {
+	return sequence(
+		'browser-sync-psi',
+  	'ngrok-url',
+  	'psi-desktop',
+  	'psi-mobile',
+  	cb);
+});
+
+gulp.task('psi', ['psi-seq'], function() {
+  console.log("finished psi");
+	process.exit();
+});
+
+gulp.task('psi-remote', ['psi-github'], function() {
+  process.exit();
+});
+
+
+gulp.task('clean', function() {
+  var del = require('del');
+  del(siteDir);
+});
+
+gulp.task('assets', ['img', 'css', 'js']);
+
+gulp.task('default', function() {
+	return sequence('clean', 'assets', 'html');
+});
+
+function errorHandler (error) {
+  console.log(error.toString());
+  this.emit('end');
+}
--- js/model.js
+++ js/model.js
@@ -20,7 +20,8 @@ function LocationsDataModel() {
     {
       name: "Wally Weekes Pool",
       suburb: "North Bondi",
-      img: "img/Wally-Weekes.jpg",
+      img: "img/Wally-Weekes.png",
+      rockPool: true,
       info: "A very popular pool to take the kids as it’s a tidal pool that was transformed into a rock pool.",
       lat: -33.891492,
       lng: 151.282316
@@ -28,7 +29,8 @@ function LocationsDataModel() {
     {
       name: "Bondi Baths",
       suburb: "Bondi",
-      img: "http://getconnected.dnsw.com.au/multimedia/NSWMainThumbnail___9612497_TNSW_dnsw999734396.jpg",
+      img: "img/Bondi-Baths.png",
+      rockPool: false,
       info: "The Bondi Baths are the home of the Bondi Icebergs Winter Swimming Club and is located at the southern end of Bondi Beach and have been a land mark for over 100 years.",
       lat: -33.895028,
       lng: 151.274570
@@ -36,16 +38,17 @@ function LocationsDataModel() {
     {
       name: "Bronte Baths",
       suburb: "Bronte",
-      img: "img/Bronte-Baths.jpg",
+      img: "img/Bronte-Baths.png",
+      rockPool: true,
       info: "Probably the most photographed of all of Sydney’s rock pools. Just along the Southern Coastal Walk. Bronte park as working bbqs a wonderful place to go for a evening swim then bbq with friends afterwards.",
       lat: -33.905049,
       lng: 151.269079
     },
-  // Images and infor from http://www.randwick.nsw.gov.au/facilities-and-recreation/beaches-and-coast/ocean-pools
     {
       name: "Clovelly Ocean Pool",
       suburb: "Clovelly",
-      img: "img/400x300_Clovelly-Pool.jpg",
+      img: "img/Clovelly-Pool.png",
+      rockPool: false,
       info: "A saltwater lap pool located next to Clovelly Beach on the concrete promenade.",
       lat: -33.914189,
       lng: 151.266941
@@ -53,7 +56,8 @@ function LocationsDataModel() {
     {
       name: "Giles Baths",
       suburb: "Coogee",
-      img: "img/400x300_Giles-Baths.jpg",
+      img: "img/Giles-Baths.png",
+      rockPool: true,
       info: "A popular swimming hole with young people and a fun place to explore from Coogee Beach. It’s located at the foot of the northern headland.",
       lat: -33.920017,
       lng: 151.260641
@@ -61,7 +65,8 @@ function LocationsDataModel() {
     {
       name: "Ross Jones Memorial Pool",
       suburb: "Coogee",
-      img: "img/400x300_Ross-Jones.jpg",
+      img: "img/Ross-Jones.png",
+      rockPool: false,
       info: "Located at the southern end of Coogee Beach, this man-made ocean pool is lots of fun for the family and gets crowded on a hot summer’s day.",
       lat: -33.922878,
       lng: 151.257858
@@ -69,7 +74,8 @@ function LocationsDataModel() {
     {
       name: "McIver's Baths",
       suburb: "Coogee",
-      img: "img/400x300_McIvers.jpg",
+      img: "img/McIvers.png",
+      rockPool: false,
       info: "The last remaining women’s-only seawater baths in Australia. There's a large concrete sea pool, brick sunbathing area, amenities block, change rooms and small clubhouse.",
       lat: -33.924103,
       lng: 151.258390
@@ -77,7 +83,8 @@ function LocationsDataModel() {
     {
       name: "Wylie's Bath",
       suburb: "Coogee",
-      img: "img/400x300_Wylies.jpg",
+      img: "img/Wylies.png",
+      rockPool: false,
       info: "An iconic sight on the Coogee ocean landscape, Wylies Baths was built in 1907 and enjoys stunning views across Coogee Bay.",
       lat: -33.925476,
       lng: 151.259100
@@ -85,7 +92,8 @@ function LocationsDataModel() {
     {
       name: "Ivor Rowe Rockpool",
       suburb: "South Coogee",
-      img: "img/400x300_Ivor-Rowe.jpg",
+      img: "img/Ivor-Rowe.png",
+      rockPool: true,
       info: "One of the smallest ocean pools along the Coastal Walkway. It is a natural, shallow rock pool and perfect for wading or cooling down on a hot day.",
       lat: -33.933177,
       lng: 151.261701
@@ -93,7 +101,8 @@ function LocationsDataModel() {
     {
       name: "Mahon Pool",
       suburb: "Maroubra",
-      img: "img/400x300_Mahon-Pool.jpg",
+      img: "img/Mahon-Pool.png",
+      rockPool: true,
       info: "The Mahon rock pool is located to the north of Maroubra Beach at the base of Jack Vanny Reserve. The exposed rock outcrops and cliffs above make it a spectacular venue and it is the home of the Maroubra Seals Winter Swimming Club.",
       lat: -33.942808,
       lng: 151.263836
@@ -101,7 +110,8 @@ function LocationsDataModel() {
     {
       name: "Malabar Pool",
       suburb: "Malabar",
-      img: "img/400x300_Malabar-Ocean-Pool.jpg",
+      img: "img/Malabar-Ocean-Pool.png",
+      rockPool: false,
       info: "Malabar Ocean Pool is located near Malabar Beach in Long Bay directly below Randwick Golf Club. It has spectacular views over Long Bay and onto the Randwick Rifle Range.",
       lat: -33.968516,
       lng: 151.254552
@@ -112,6 +122,8 @@ function LocationsDataModel() {
 function LocationsViewModel(data) {
   'use strict';
   var self = this;
+
+  self.data = data;
 
   self.selectedLocation = ko.observable();
 
@@ -124,8 +136,8 @@ function LocationsViewModel(data) {
     /* Include locations if start of name or suburb matches the filter text */
     return ko.utils.arrayFilter(locations, function(location) {
       return (includeAll ||
-        ko.utils.stringStartsWith(location.name.toLowerCase(), filterText) ||
-        ko.utils.stringStartsWith(location.suburb.toLowerCase(), filterText));
+        location.name.toLowerCase().indexOf(filterText) === 0 ||
+        location.suburb.toLowerCase().indexOf(filterText) == 0);
     });
   }).extend( { throttle: 100 });
 
@@ -147,7 +159,7 @@ function LocationsViewModel(data) {
     }
     this.selected(true);
     self.selectedLocation(this);
-  }
+  };
 
   self.setSelected = function(location) {
     self.selectedLocation(location);
@@ -174,7 +186,7 @@ function LocationsViewModel(data) {
       offset.left = $list.width() + 1;
     }
     return offset;
-  }
+  };
 
   // Add extra info for for each location
   for (var i in data.locations) {
@@ -209,7 +221,12 @@ function MapViewModel(viewModel) {
 
     // Ensure map & infoWindow are centred on location
     self.map.setCenter(location.marker.position);
-    self.infoWindow.open(self.map, location.marker);
+    // Bounce once if selected
+    location.marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+      location.marker.setAnimation(null);
+      self.infoWindow.open(self.map, location.marker);
+    }, 700);
   });
 
   /*
@@ -313,13 +330,14 @@ function FlickrViewModel(viewModel) {
   self.imageArray = ko.observableArray([]);
   self.viewModel = viewModel;
 
+  self.errorMessage = ko.observable('');
   self.flickr = new Flickr({
     api_key: '562af0fb090c53b310b934fef0c87a7d'
   });
 
   self.currentWidth = function() {
     return Number($('#flickr-view').css('width').slice(0, -2));
-  }
+  };
 
   self.triggerResize = ko.observable();
 
@@ -345,15 +363,15 @@ function FlickrViewModel(viewModel) {
     self.imageArray.removeAll();
 
     self.flickr.photos.search({
-      text: location.name, // + ' Australia',
+      text: location.name,
       lat: location.lat,
       lon: location.lng,
       radius: 0.5,
       geocontext: 2 // outdoors
     }, function(err, result) {
       if (err) {
-        console.log(err);
-        $('#flickr-heading').text('Flickr Images for ' + location.name +  'could not be loaded: ' + err);
+        self.errorMessage('Flickr Images for ' + location.name +  'could not be loaded.');
+        console.log(err); // TODO???
       }
       else {
          var imageInfo = [];
@@ -399,35 +417,66 @@ function WeatherViewModel(viewModel) {
   var self = this;
 
   self.viewModel = viewModel;
-/*
-  self.forecast = new Forecast({
-    APIKey: '2e2344aac39eeec30969f17aff17d447',
-    timeout: 1000
-  });
-*/
+
+  self.errorMessage = ko.observable('');
+
+  self.temperature = ko.observable();
+  self.apparentTemperature = ko.observable();
+  self.dewPoint = ko.observable();
+  self.humidity = ko.observable();
+  self.pressure = ko.observable();
+  self.summary = ko.observable();
+  self.time = ko.observable();
+  self.precipProbability = ko.observable();
+  self.imgSrc = ko.observable();
+  self.nextDay = ko.observable();
+  self.nextWeek = ko.observable();
+
   viewModel.selectedLocation.subscribe(function(location) {
-    /*
-    forecast.get(location.lat, location.lng, function(err, res, data) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        log('res: ' + util.inspect(res));
-        log('data: ' + util.inspect(data));
+
+    var requestTimeout = setTimeout(function() {
+        self.errorMessage('Failed to get weather information for ' + location.name);
+    }, 10);
+
+    $.ajax({
+      url: 'https:///api.forecast.io/forecast/2e2344aac39eeec30969f17aff17d447/' + location.lat + ',' + location.lng +
+        '?units=auto', //&exclude=daily,hourly,minutely,alerts',
+      dataType: 'jsonp',
+      success: function(data) {
+
+        console.log(data); // TODO: Remove
+
+        var imperial = (data.flags.units === 'us');
+        var temperatureUnits = '°C';
+        var pressureUnits = 'hPa';
+
+        if (imperial) {
+          temperatureUnits = '°F';
+          pressureUnits = 'mb';
+        }
+        var currently = data.currently;
+
+        self.temperature(currently.temperature.toFixed(1) + temperatureUnits);
+        self.apparentTemperature(currently.apparentTemperature.toFixed(1) + temperatureUnits);
+        self.dewPoint(currently.dewPoint.toFixed(1) + temperatureUnits)
+
+        self.summary(currently.summary);
+
+        // Convert to percentages
+        self.humidity(Number(currently.humidity) * 100.0 + '%');
+        self.precipProbability(Number(currently.precipProbability) * 100.0 + '%');
+
+        self.pressure(currently.pressure + pressureUnits);
+
+        var dt = new Date();
+        dt.setTime(Number(currently.time) * 1000);
+        self.time(dt.toDateString() + ' ' + dt.toLocaleTimeString());
+
+        self.imgSrc('img/' + currently.icon + '.png');
+        self.nextDay(data.hourly.summary);
+        self.nextWeek(data.daily.summary);
       }
     });
-
-    var url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + location.lat + '&lon=' + location.lng +
-       '&appid=' +
-       '&callback=';
-    $.ajax(url, {
-      type: 'GET',
-      datatype: 'jsonp',
-      error: function(e) {
-        console.log(e.status + ' ' + e.statusText);
-      }
-    })
-*/
   });
 
   self.resize = function() {
