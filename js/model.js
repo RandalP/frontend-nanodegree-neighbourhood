5c7c774a19f73b50805c0e7b916a5ef3a3b00af4
Randal Parsons <randal_parsons@yahoo.com>
2015-03-02 15:27:30 +1100

    Add jsdoc. Implement check-box for rock pools and man-made pools.

--- js/model.js
+++ js/model.js
@@ -9,8 +9,9 @@ ko.bindingHandlers.stopBinding = {
 };
 ko.virtualElements.allowedBindings.stopBinding = true;
 
-/*
- * Locations DataModel
+/**
+ * Locations DataModel.
+ * @constructor
  */
 function LocationsDataModel() {
   'use strict';
@@ -119,39 +120,101 @@ function LocationsDataModel() {
   ];
 }
 
+/**
+ * View model for the list of locations.
+ * @constructor
+ * @param {Object} data - the LocationsDataModel.
+ */
 function LocationsViewModel(data) {
   'use strict';
   var self = this;
 
   self.data = data;
 
+  // Current selected location
   self.selectedLocation = ko.observable();
 
+  // Filtering functionality
   self.filterText = ko.observable('');
+  self.rockPool = ko.observable(true);
+  self.manMade = ko.observable(true);
 
+  // List of locations currently unfiltered.
   self.locationArray = ko.pureComputed(function() {
     var locations = data.locations;
     var filterText = self.filterText().toLowerCase();
-    var includeAll = filterText.length === 0;
+    var noTextSearch = filterText.length === 0;
+
+    // Filter based on name and then type (rockPool/manMade)
     // Include locations if start of name or suburb matches the filter text
     return ko.utils.arrayFilter(locations, function(location) {
-      return (includeAll ||
-        location.name.toLowerCase().indexOf(filterText) === 0 ||
-        location.suburb.toLowerCase().indexOf(filterText) == 0);
+      return (noTextSearch ||
+                location.name.toLowerCase().indexOf(filterText) === 0 ||
+                location.suburb.toLowerCase().indexOf(filterText) === 0) &&
+              ((self.rockPool() && location.rockPool) ||
+               (self.manMade() && !location.rockPool));
     });
   }).extend( { throttle: 100 });
 
+  /**
+   * Launch the flickr dialog.
+   * @method LocationsViewModel#displayFlickr
+   */
   self.displayFlickr = function() {
     window.location.href = '#flickr';
     flickrViewModel.resize();
   };
 
+  /**
+   * Launch the weather dialog.
+   * @method LocationsViewModel#displayWeather
+   */
   self.displayWeather = function() {
     window.location.href = '#weather';
     weatherViewModel.resize();
   };
 
-  self.selectLocation = function() {
+  /**
+   * Selects the specified location.
+   * @method LocationsViewModel#setSelected
+   * @param {Object} location - the location to select.
+   */
+  self.setSelected = function(location) {
+    self.selectedLocation(location);
+  };
+
+  /**
+   * Resizes the location list appropriately.
+   * @method LocationsViewModel#resize
+   */
+  self.resize = function() {
+    var $list = $('#location-list');
+    $list.height($(window).height() - $list.offset().top);
+  };
+
+  /**
+   * Returns the position of the list's top right corner.
+   * @method LocationsViewModel#topRight
+   * @returns {number} The top right of the list.
+   */
+  self.topRight = function() {
+    var $list = $('#location-list');
+    var offset = $list.offset();
+
+    // Now that list has been sized, make the search match.
+    $('#search').width($list.width() - 5);
+    if (offset.left < 0) {
+      offset.left = 0;
+    } else {
+      offset.left = $list.width() + 1;
+    }
+    return offset;
+  };
+
+  /*
+   * Selects the currently bound location.
+   */
+  var selectLocation = function() {
     var oldLocation = self.selectedLocation();
     if (oldLocation) {
       // Clean up the previous selection.
@@ -161,27 +224,6 @@ function LocationsViewModel(data) {
     self.selectedLocation(this);
   };
 
-  self.setSelected = function(location) {
-    self.selectedLocation(location);
-  };
-
-  self.resize = function() {
-    var $list = $('#location-list');
-    $list.height($(window).height() - $list.offset().top);
-  };
-
-  self.topRight = function() {
-    var $list = $('#location-list');
-    var offset = $list.offset();
-
-    if (offset.left < 0) {
-      offset.left = 0;
-    } else {
-      offset.left = $list.width() + 1;
-    }
-    return offset;
-  };
-
   // Add extra info for for each location
   for (var i in data.locations) {
     var location = data.locations[i];
@@ -189,15 +231,17 @@ function LocationsViewModel(data) {
     location.ordinal = Number(i) + 1;
     location.selected = ko.observable(false);
     location.parent = self;
-    location.select = self.selectLocation.bind(location);
+    location.select = selectLocation.bind(location);
   }
 
   // Force initial sizing.
   self.resize();
 }
 
-/*
- * Map ViewModel
+/**
+ * Map ViewModel.
+ * @constructor
+ * @param {Object} viewModel - the LocationsViewModel
  */
 function MapViewModel(viewModel) {
   'use strict';
@@ -205,10 +249,12 @@ function MapViewModel(viewModel) {
   var self = this;
   self.viewModel = viewModel;
 
+  // Defer initialization of map until DOM is loaded.
   google.maps.event.addDomListener(window, 'load', function() {
     self.initializeMap();
   });
 
+  // Suscribe to changes in selected location to update marker and info window.
   viewModel.selectedLocation.subscribe(function(location) {
     // Close previous infoWindow
     self.infoWindow.close();
@@ -219,14 +265,13 @@ function MapViewModel(viewModel) {
     location.marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function() {
       location.marker.setAnimation(null);
-      // Pause to reduce jerky transition from animation to window opening.
-      setTimeout(function(){}, 100);
-      self.infoWindow.open(self.map, location.marker);
     }, 700);
+    self.infoWindow.open(self.map, location.marker);
   });
 
-  /*
-   * Initialize the Google Map.
+  /**
+   * Initializes the Google Map with markers and info window.
+   * @method MapViewModel#initializeMap
    */
   self.initializeMap = function() {
 
@@ -237,6 +282,7 @@ function MapViewModel(viewModel) {
 
     self.map = new google.maps.Map($('#map').get(0), mapOptions);
 
+    // Load template into info window.
     self.infoWindow = new google.maps.InfoWindow({
       content: $('#info-template').html()
     });
@@ -251,6 +297,7 @@ function MapViewModel(viewModel) {
 
     var bounds = window.mapBounds = new google.maps.LatLngBounds();
 
+    // Place a pin at the specified location.
     function pinLocation(ordinal, location) {
 
       location.marker = new google.maps.Marker({
@@ -276,11 +323,17 @@ function MapViewModel(viewModel) {
       ++i;
     });
 
+    // Subscribe to changes in locationArray so we can respond to filtering.
     self.viewModel.locationArray.subscribe(function(filteredLocations) {
+      function matchLocation(loc) {
+        /* jshint validthis:true */
+        return (loc.ordinal === this.ordinal);
+      }
+
       // Remove map from all locations which have been filtered out.
       for (var l = 0, nLocation = self.locations.length; l < nLocation; l++) {
         var location = self.locations[l];
-        var map = ko.utils.arrayFirst(filteredLocations, function(loc) { return loc.ordinal === location.ordinal; }) ? self.map : null;
+        var map = ko.utils.arrayFirst(filteredLocations, matchLocation.bind(location)) ? self.map : null;
         location.marker.setMap(map);
       }
     });
@@ -289,14 +342,21 @@ function MapViewModel(viewModel) {
     self.resize();
   };
 
-  /*
-   * Selects a given location
+  /**
+   * Selects the specified location.
+   * @method MapViewModel#setSelected
+   * @param {Object} location - the location to select.
    */
   self.setSelected = function(location) {
     self.viewModel.selectedLocation(location);
     google.maps.event.trigger(self.map, 'resize');
   };
 
+  /**
+   * Resizes the map displayed in the browser and sets both the bounds and center.
+   * @method MapViewModel#resize
+   * @param {Object} [resizeEvent] - an event, if triggered by browser resizing.
+   */
   self.resize = function(resizeEvent) {
     var $map_view = $('.map-view');
     var offset = viewModel.topRight();
@@ -307,6 +367,7 @@ function MapViewModel(viewModel) {
 
     if (window.mapBounds) {
       // If triggered by a resize event, we don't need to fire the event again.
+      // The rest of the time, we're using this to trick the map into redrawing properly.
       if (!resizeEvent) {
         google.maps.event.trigger(self.map, 'resize');
       }
@@ -317,27 +378,35 @@ function MapViewModel(viewModel) {
 }
 
 /**
- * FlickrViewModel
+ * Flickr image ViewModel.
+ * @constructor
+ * @param {Object} viewModel - the LocationsViewModel
  */
 function FlickrViewModel(viewModel) {
   'use strict';
   var self = this;
 
-  self.imageArray = ko.observableArray([]);
   self.viewModel = viewModel;
+  self.imageArray = ko.observableArray([]);
 
   self.errorMessage = ko.observable('');
   self.flickr = new Flickr({
     api_key: '562af0fb090c53b310b934fef0c87a7d'
   });
 
+  /**
+   * Calculates the current width of the div containing the images.
+   * @method FlickrViewModel#currentWidth
+   * @returns {number} The current width of #flickr-view.
+   */
   self.currentWidth = function() {
     return Number($('#flickr-view').css('width').slice(0, -2));
   };
 
+  // Flag used to force resizing of flickr image source.
   self.triggerResize = ko.observable();
 
-  // Create observable so we can adjust as screen resizes.
+  // Contains the code used to determine flickr image sizes in the source strings.
   self.sizeCode = ko.computed(function() {
     // Image size should be based on size of #flickr-view
     self.triggerResize();
@@ -354,6 +423,7 @@ function FlickrViewModel(viewModel) {
     return sizeCode;
   });
 
+  // Subscribe to currently selected location to trigger ajax request.
   viewModel.selectedLocation.subscribe(function(location) {
 
     self.imageArray.removeAll();
@@ -365,8 +435,15 @@ function FlickrViewModel(viewModel) {
       radius: 0.5,
       geocontext: 2 // outdoors
     }, function(err, result) {
+
+      function getSrc() {
+        /* jshint validthis:true */
+        return 'https://farm' + this.farm + '.staticflickr.com/' + this.server + '/' +
+          this.id + '_' + this.secret + '_' + self.sizeCode() + '.jpg';
+      }
+
       if (err) {
-        self.errorMessage('Flickr Images for ' + location.name +  'could not be loaded.');
+        self.errorMessage('Flickr Images could not be loaded.');
       } else {
         // Clear error
         self.errorMessage('');
@@ -374,40 +451,46 @@ function FlickrViewModel(viewModel) {
         var imageInfo = [];
         for (var i in result.photos.photo) {
           var photo = result.photos.photo[i];
+
           var info = {
             url: ko.observable('https://www.flickr.com/photos/' + photo.owner + '/' + photo.id),
-            src: ko.computed(function() {
-              return 'https://farm' + this.farm + '.staticflickr.com/' + this.server + '/' +
-                this.id + '_' + this.secret + '_' + self.sizeCode() + '.jpg';
-            }, photo)
+            src: ko.computed(getSrc, photo)
           };
           imageInfo.push(info);
         }
+        // Update array in one operation for efficiency.
         ko.utils.arrayPushAll(self.imageArray, imageInfo);
         self.imageArray.valueHasMutated();
       }
     });
   });
 
+  /**
+   * Resizes the dialog box display.
+   * @method FlickrViewModel#resize
+   */
   self.resize = function() {
-    var $flickr_view = $('#flickr-view');
-    var $flickr_images = $('#flickr-images');
+    if (self.errorMessage().length === 0) {
+      var $flickr_view = $('#flickr-view');
+      var $flickr_images = $('#flickr-images');
 
-    var padding = $flickr_images.outerHeight() - $flickr_images.height();
-    var viewOffset = $flickr_view.offset().top;
-    var imageOffset = $flickr_images.offset().top;
-    var height = $(window).height() - 2 * viewOffset;
+      var padding = $flickr_images.outerHeight() - $flickr_images.height();
+      var viewOffset = $flickr_view.offset().top;
+      var imageOffset = $flickr_images.offset().top;
+      var height = $(window).height() - 2 * viewOffset;
 
-    $flickr_view.height(height);
-    $flickr_images.height(height - imageOffset + viewOffset);
-    // Trigger image resizing.
-    self.triggerResize.notifySubscribers();
+      $flickr_view.height(height);
+      $flickr_images.height(height - imageOffset + viewOffset);
+      // Trigger image resizing.
+      self.triggerResize.notifySubscribers();
+    }
   };
 }
 
-
 /**
- * WeatherViewModel
+ * Weather info ViewModel.
+ * @constructor
+ * @param {Object} viewModel - the LocationsViewModel
  */
 function WeatherViewModel(viewModel) {
   'use strict';
@@ -429,11 +512,12 @@ function WeatherViewModel(viewModel) {
   self.nextDay = ko.observable();
   self.nextWeek = ko.observable();
 
+  // Subscribe to change in selected location to trigger ajax request.
   viewModel.selectedLocation.subscribe(function(location) {
 
     var requestTimeout = setTimeout(function() {
-        self.errorMessage('Failed to get weather information for ' + location.name);
-    }, 100);
+        self.errorMessage('Failed to retrieve weather information');
+    }, 500);
 
     $.ajax({
       url: 'https:///api.forecast.io/forecast/2e2344aac39eeec30969f17aff17d447/' + location.lat + ',' + location.lng +
@@ -454,46 +538,76 @@ function WeatherViewModel(viewModel) {
         }
         var currently = data.currently;
 
-        self.temperature(currently.temperature.toFixed(1) + temperatureUnits);
-        self.apparentTemperature(currently.apparentTemperature.toFixed(1) + temperatureUnits);
-        self.dewPoint(currently.dewPoint.toFixed(1) + temperatureUnits)
+        // Convert the provided number string to a temperature string.
+        function toTemperature(number) {
+          return number.toFixed(1) + temperatureUnits;
+        }
 
+        // Convert the provided number string to a percentage string.
+        function toPercentage(number) {
+          return Number(number) * 100.0 + '%';
+        }
+
+        self.imgSrc('img/' + currently.icon + '.png');
+        self.temperature(toTemperature(currently.temperature));
         self.summary(currently.summary);
-
-        // Convert to percentages
-        self.humidity(Number(currently.humidity) * 100.0 + '%');
-        self.precipProbability(Number(currently.precipProbability) * 100.0 + '%');
-
+        self.apparentTemperature(toTemperature(currently.apparentTemperature));
+        self.humidity(toPercentage(currently.humidity));
+        self.dewPoint(toTemperature(currently.dewPoint));
+        self.precipProbability(toPercentage(currently.precipProbability));
         self.pressure(currently.pressure + pressureUnits);
 
         var dt = new Date();
         dt.setTime(Number(currently.time) * 1000);
         self.time(dt.toDateString() + ' ' + dt.toLocaleTimeString());
 
-        self.imgSrc('img/' + currently.icon + '.png');
         self.nextDay(data.hourly.summary);
         self.nextWeek(data.daily.summary);
       }
     });
   });
 
+  /**
+   *  Resizes the weather dialog.
+   * @method WeatherViewModel#resize
+   */
   self.resize = function() {
   };
 }
 
-/*
- * Data Model
+/**
+ * Locations Data Model.
+ * @global
  */
 var locationsDataModel = new LocationsDataModel();
 
-/*
- * View Models
+/**
+ * Locations View Model
+ * @global
  */
+
 var locationsViewModel = new LocationsViewModel(locationsDataModel);
+/**
+ * Map View Model
+ * @global
+ */
+
 var mapViewModel = new MapViewModel(locationsViewModel);
+/**
+ * Flickr View Model
+ * @global
+ */
 var flickrViewModel = new FlickrViewModel(locationsViewModel);
+
+/**
+ * Weather View Model
+ * @global
+ */
 var weatherViewModel = new WeatherViewModel(locationsViewModel);
 
+/*
+ * Subscribe to window resize events and propagate to view models.
+ */
 google.maps.event.addDomListener(window, 'resize', function(e) {
   locationsViewModel.resize();
   mapViewModel.resize(e);
@@ -525,3 +639,10 @@ $('#close-weather').click(function() {
   window.history.back();
 });
 
+/*
+ * Apply Knockout bindings.
+ */
+ko.applyBindings(locationsViewModel, document.getElementById("header"));
+ko.applyBindings(locationsViewModel, document.getElementById("location-list"));
+ko.applyBindings(flickrViewModel, document.getElementById("flickr"));
+ko.applyBindings(weatherViewModel, document.getElementById("weather"));
